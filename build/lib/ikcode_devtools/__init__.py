c8a960cadeaf0ec01849adeb19e33f604f77991c
Yusuf Darish <yusufdarwish70@gmail.com>
2025-05-23 18:13:29 -0700

    commit6

--- build/lib/ikcode_devtools/__init__.py
+++ build/lib/ikcode_devtools/__init__.py
@@ -1,5 +1,6 @@
 from .main import MainWindow, runGUI, CheckInfo, Help, getVersion
 from ikcode_devtools.inspector import getInspect, inspection_results
+from ikcode_devtools.gtest import gTest, gtest_registry
 
 __all__ = [
     "MainWindow",
